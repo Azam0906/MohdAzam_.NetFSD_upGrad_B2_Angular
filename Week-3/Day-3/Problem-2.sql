11910fe88ea62422a988c00e8d48b6762224167c
Mohd Azam <azam.khan0906@gmail.com>
2026-03-05 12:29:28 +0530

    updated

--- Week-3/Day-3/Problem-2.sql
+++ Week-3/Day-3/Problem-2.sql
@@ -1,9 +1,4 @@
-SELECT 
-    p.product_name, 
-    b.brand_name, 
-    c.category_name, 
-    p.model_year, 
-    p.list_price
+SELECT p.product_name, b.brand_name, c.category_name, p.model_year, p.list_price
 FROM products p
 INNER JOIN brands b ON p.brand_id = b.brand_id
 INNER JOIN categories c ON p.category_id = c.category_id
